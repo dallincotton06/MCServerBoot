217f890612ff9882be229199eb8f4f62ed2cf3f1
dallincotton06 <133382698+dallincotton06@users.noreply.github.com>
2025-04-20 21:44:14 -0600

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,9 +1,9 @@
-cd /home/ubuntu/meatballcraft
+cd ~/Server
 currentdir=$(pwd)
 
 while true; do
   if [ -z $(curl -Is http://169.254.169.254/latest/meta-data/spot/termination-time | head -1 | grep 404 | cut -d ' ' -f 2) ]; then
-    sudo aws s3 sync $currentdir s3://meatball-craft/server/
+    sudo aws s3 sync $currentdir s3://your-s3-bucket #UPDATE
       break
         else
           sleep 5
